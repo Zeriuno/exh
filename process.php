c803dd84ab755c2ad54c9508c7b0adaa05ce1c4d
Zeriuno <d.pitrolo@gmx.com>
2016-02-26 01:55:20 +0100

    Ajout réussi

--- process.php
+++ process.php
@@ -1,63 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equi="Content-Type" content="text/html" charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fais ta carte!</title>
+  </head>
+  <body>
+    <main>
+      <header>
+        <a href="index.html" title="">Accueil</a>
+      </header>
+      <form method="post" action="test.php">
+        <label for="prenom">Prénom:
+          <input type="text" name="prenom" placeholder="Victor" required>
+        </label>
+        <label for="nom">Nom:
+          <input type="text" name="nom" placeholder="Hugo" required>
+        </label>
+        <label for="email"><abbr title="Messagerie électronique en ligne">Mél</abbr>:
+          <input type="text" name="email" multiple placeholder="auteur@lalegendedessiecl.es" pattern="([\w*\d*\.*\-*\_*]*)(\+\w*\d*)?@[\w+\.\w+]*" required>
+        </label>
+        <label for="telephone">Téléphone:
+          <input type="text" name="tel" placeholder="+33 1793" pattern="(\+\d{1,2})?[\d+\s*]+">
+        </label>
+        <button type="submit">C'est bon</button>
+      </form>
 
-// Récupération des variables
+      <img src="eg.png" alt="" />
+      <?php
+          try{ $pdo = new PDO('mysql:host=localhost;dbname=cartes;charset=utf8', 'root', 'root');
+          }
+          catch (Exception $e) {
+           die('Erreur : ' . $e->getMessage());
+          }
+          $stmt = $pdo->prepare('INSERT INTO visiteurs(prenom, nom, mel, tel) VALUES(:prenom, :nom, :mel, :tel)');
+          $stmt->bindParam(':prenom', $_POST['prenom']);
+          $stmt->bindParam(':nom', $_POST['nom']);
+          $stmt->bindParam(':mel', $_POST['email']);
+          $stmt->bindParam(':tel', $_POST['tel']);
+          $stmt->execute();
+          echo 'Données enregistrées';
 
-  if(isset($_POST['nom'])) $lname=$_POST['nom']         ;
-  else $nom=""                                        ;
-  if(isset($_POST['prenom'])) $fname=$_POST['prenom'];
-  else $prenom=""                                     ;
-  if(isset($_POST['email'])) $mail=$_POST['email']  ;
-  else $email=""                                      ;
-  if(isset($_POST['tel'])) $phone = $_POST['tel']       ;
+        ?>
+      <?php
+         $nom_image = "eg.png";
+         header("Content-type: image/png");
+         $image = imagecreatefrompng("eg.png");
+         $noir = imagecolorallocate($image, 0, 0, 0);
+         imagestring($image, 5, 100, 25, $_POST['prenom'], $noir);
+         imagestring($image, 5, 200, 25, $_POST['nom'], $noir);
+         imagestring($image, 5, 100, 50, $_POST['email'], $noir);
+         imagestring($image, 5, 100, 75, $_POST['tel'], $noir);
+         imagepng($image, "carte.png");
+         ?>
 
-
-  // test des champs vides
-
-  if(empty($lname) OR empty($fname) OR empty($mail))
-  {
-    echo '<strong>Attention, tous les champs sont obligatoires. Seul le numéro de téléphone est facultatif.</strong>';
-  }
-  else //on y va
-  {
-    //BDD
-
-    //Déterminer les variables pour la base: port, nom utilisateur et mot de passe
-      $dbhost = 'localhost:3306';
-      $db = 'cartes';
-      $dbuser = 'root';
-      $dbpass = 'root';
-      $charset = 'utf8';
-      $dsn = "mysql:host=$dbhost;dbname=$db;charset=$charset";
-
-      $opt = [
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false,
-      ];
-      $pdo = new PDO($dsn, $dbuser, $dbpass, $opt);   //Ouverture de la connexion avec la base
-
-      $stmt = $pdo->prepare('INSERT INTO visiteurs './/nom de la table
-            '(prenom, nom, mel, tel) '. //nom des colonnes
-            'VALUES ( :prenom, :nom, :email, :tel )'; //placeholders
-            )
-      $stmt = execute(['prenom' => $fname, 'nom' => $lname, 'mel' => $mail, 'tel' => $phone]);
-/*
-    //Image
-
-    $image = imagecreatefrompng("eg.png");
-
-    //écriture de l'image
-
-
-    imagepng($image);
-    <img src="image.php" />
-
-      header()
-      mail( "$email", "Fais ta carte! - C'est fait!",
-        $message, "From: $email" );
-      header( "Location: http://www.example.com/thankyou.html" );
-      */
-  }
-
-  register_shutdown_function('shutdown');
-?>
+      </main>
+    </body>
+  </html>
